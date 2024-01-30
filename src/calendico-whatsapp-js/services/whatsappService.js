290c0ac21aca8d221168a2658413f460bd2725f7
mgomez11 <miguelgomez66@gmail.com>
2024-01-30 18:55:47 -0300

    add log for debugging

--- src/calendico-whatsapp-js/services/whatsappService.js
+++ src/calendico-whatsapp-js/services/whatsappService.js
@@ -42,9 +42,7 @@ const store = new AwsS3Store({
 const initializeWhatsAppClient = async (location_identifier, user_id) => {
     console.log('=====================');
     console.log('railsAppBaseUrl');
-    console.log(process.env.RAILS_APP_URL);
-    console.log(process.env.RAILS_APP_ENDPOINT_SCOPE);
-    console.log(process.env.RAILS_APP_URL + process.env.RAILS_APP_ENDPOINT_SCOPE);
+    console.log(railsAppBaseUrl);
     console.log('=====================');
     console.log(`Initializing WhatsApp client for ${location_identifier} by user ${user_id}...`);
     addClientInitializing(location_identifier, true);
