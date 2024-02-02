71348a43b04ab245db023057434a0cd0b720438a
mgomez11 <miguelgomez66@gmail.com>
2024-02-02 08:50:08 -0300

    improve naming

--- src/calendico-whatsapp-js/endpoints/routes.js
+++ src/calendico-whatsapp-js/endpoints/routes.js
@@ -8,8 +8,8 @@ const { getContacts } = require('../services/getContacts');
 
 // Endpoint to send a message
 router.post('/send_message', async (req, res) => {
-    const { location_identifier, receiver_phone, message, message_id, sending_otp } = req.body;
-    return await send_message_to_client(location_identifier, res, receiver_phone, message, message_id, sending_otp);
+    const { location_identifier, receiver_phone, message, message_id, dont_preview_links } = req.body;
+    return await send_message_to_client(location_identifier, res, receiver_phone, message, message_id, dont_preview_links);
 });
 
 router.post('/login', (req, res) => {
