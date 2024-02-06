28689a3901217d193ab0dc6f5409333275162e5d
Matias Carpintini <carpintinimatias@gmail.com>
2024-02-06 17:58:23 -0300

    await 5s to archive

--- src/calendico-whatsapp-js/services/sendMessageToClient.js
+++ src/calendico-whatsapp-js/services/sendMessageToClient.js
@@ -1,80 +1,111 @@
-const { initializeWhatsAppClient } = require('./whatsappService');
-const { getClient } = require('./../clients/ClientsConnected');
-const { addClientInitializing, getClientInitializing } = require('../clients/ClientsInitializingSession');
+const { initializeWhatsAppClient } = require("./whatsappService");
+const { getClient } = require("./../clients/ClientsConnected");
+const {
+    addClientInitializing,
+    getClientInitializing,
+} = require("../clients/ClientsInitializingSession");
 
-async function send_message_to_client(location_identifier, res, receiver_phone, message, message_id, dont_preview_links) {
+async function send_message_to_client(
+    location_identifier,
+    res,
+    receiver_phone,
+    message,
+    message_id,
+    dont_preview_links
+) {
     try {
         const client = getClient(location_identifier);
         if (!client || getClientInitializing(location_identifier)) {
             if (getClientInitializing(location_identifier)) {
-                console.log('============================================');
-                console.log('Client is already initializing...');
-                console.log('============================================');
+                console.log("============================================");
+                console.log("Client is already initializing...");
+                console.log("============================================");
                 return res.status(400).json({
                     success: false,
-                    message: 'Client is already initializing. Please try again in a few seconds.'
+                    message:
+                        "Client is already initializing. Please try again in a few seconds.",
                 });
             } else {
-                console.log('============================================');
-                console.log('Client not initialized yet. Initializing...');
-                console.log('============================================');
-                await initializeWhatsAppClient(location_identifier, 'automatic_reconnect');
+                console.log("============================================");
+                console.log("Client not initialized yet. Initializing...");
+                console.log("============================================");
+                await initializeWhatsAppClient(
+                    location_identifier,
+                    "automatic_reconnect"
+                );
                 return res.status(400).json({
                     success: false,
-                    message: 'Client not initialized yet. Please try again after re connecting the session. We\'ll try to reconnect the session automatically.'
+                    message:
+                        "Client not initialized yet. Please try again after re connecting the session. We'll try to reconnect the session automatically.",
                 });
             }
         }
         const client_state = await client.getState().catch(async (error) => {
-            console.error('Error getting client state:', error);
+            console.error("Error getting client state:", error);
             addClientInitializing(location_identifier, client);
             await initializeWhatsAppClient(location_identifier);
-            return res.status(500).json({success: false, message: 'Error getting client state'});
+            return res
+                .status(500)
+                .json({
+                    success: false,
+                    message: "Error getting client state",
+                });
         });
-        console.log('============================================');
-        console.log('client.getState():', client_state);
-        console.log('============================================');
-        if (client_state === null || client_state === 'CONFLICT' || client_state === 'UNPAIRED' || client_state === 'UNLAUNCHED') {
+        console.log("============================================");
+        console.log("client.getState():", client_state);
+        console.log("============================================");
+        if (
+            client_state === null ||
+            client_state === "CONFLICT" ||
+            client_state === "UNPAIRED" ||
+            client_state === "UNLAUNCHED"
+        ) {
             await initializeWhatsAppClient(location_identifier);
             return res.status(400).json({
                 success: false,
-                message: 'Client session expired. Please try again in a few seconds.'
+                message:
+                    "Client session expired. Please try again in a few seconds.",
             });
         } else {
-            console.log('============================================');
-            console.log('Sending message...');
-            console.log('============================================');
+            console.log("============================================");
+            console.log("Sending message...");
+            console.log("============================================");
             try {
-                const messageObject = await client.sendMessage(`${receiver_phone}@c.us`, message, { linkPreview: (dont_preview_links == null) });
+                const messageObject = await client.sendMessage(
+                    `${receiver_phone}@c.us`,
+                    message,
+                    { linkPreview: dont_preview_links == null }
+                );
                 let chat = await messageObject.getChat();
-                console.log('============================================');
-                console.log('Message sent successfully');
-                console.log('chat:', chat.archived);
-                console.log('============================================');
-                const archive_result = await chat.archive();
-                console.log('archive_result:', archive_result);
-                console.log('chat:',  await messageObject.getChat().archived);
-                console.log('============================================');
-                res.json({ 
+                setTimeout(() => {
+                    chat.archive();
+                }, 5000);
+
+                res.json({
                     success: true,
                     from: messageObject.from,
                     to: messageObject.to,
                     message_body: messageObject.body,
-                    message: 'Message sent successfully',
+                    message: "Message sent successfully",
                     message_id: message_id,
-                    message_serialized_id: messageObject.id._serialized, 
+                    message_serialized_id: messageObject.id._serialized,
                     message_status: messageObject.ack,
-                    message_archived: archive_result 
+                    message_archived: true,
                 });
-
             } catch (error) {
-                console.error('Error:', error);
-                res.status(500).json({ success: false, message: 'Error sending message' });
+                console.error("Error:", error);
+                res.status(500).json({
+                    success: false,
+                    message: "Error sending message",
+                });
             }
         }
     } catch (error) {
         console.error(error);
-        res.status(500).json({success: false, message: 'Error sending message'});
+        res.status(500).json({
+            success: false,
+            message: "Error sending message",
+        });
     }
 }
 
